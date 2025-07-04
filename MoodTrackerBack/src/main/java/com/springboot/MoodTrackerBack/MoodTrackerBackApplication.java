e19c0257a99459e605a7537dcde9ceb5791de0f1
Jessica <diackjessica5@gmail.com>
2025-07-04 17:20:31 -0400

    initial commit

---
+++ MoodTrackerBack/src/main/java/com/springboot/MoodTrackerBack/MoodTrackerBackApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.MoodTrackerBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodTrackerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodTrackerBackApplication.class, args);
+	}
+
+}
---
+++ MoodTrackerBack/src/test/java/com/springboot/MoodTrackerBack/MoodTrackerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.MoodTrackerBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodTrackerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
